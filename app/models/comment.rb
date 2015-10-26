459d295e58e379684d41ffba5c2ae9ac62559133
Alexey Krasman <akrasman@gmail.com>
2015-10-26 12:23:49 +0300

    Fix .xls export

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,14 @@
 class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
   belongs_to :user
+
+  def self.to_text(options = {})
+  	comments = [];
+
+  	all.each do |comment|
+  		comments << "#{comment.user.name} #{comment.created_at.strftime("%d %B %Y, %H:%M")} \n#{comment.message}"
+  	end
+
+  	comments.join("\n\n")
+  end
 end
