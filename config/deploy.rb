a207522275431f06478e52afd5f9915e23c6d2f9
Alexey Krasman <akrasman@gmail.com>
2015-10-30 11:30:36 +0300

    Tune configs.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :application, 'tnd'
 set :repo_url, 'https://github.com/Iverson/tnd-m.git'
 set :deploy_to, '/www/tnd'
 set :log_level, :debug
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{tmp/sockets tmp/pids log public/system}
 # set :sockets_path, Pathname.new("#{fetch(:deploy_to)}/shared/tmp/sockets/")
 
