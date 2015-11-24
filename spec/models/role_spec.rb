cf6a86ede8ec3a24bdc737e741f45bfd969ec283
Alexey Krasman <akrasman@gmail.com>
2015-11-24 16:27:41 +0300

    Specs. Tabs -> spaces

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -2,76 +2,76 @@ require 'rails_helper'
 
 describe Role do
 
-	describe "Ability" do
+  describe "Ability" do
 
-		context	"when set all abilities" do
-			before :all do
-    		super_role = create(:role, name: 'Super role', ability: {all: {manage: "1"}})
-    		@super_user = build(:user, name: 'User', role_id: super_role.id)
+    context "when set all abilities" do
+      before :all do
+        super_role = create(:role, name: 'Super role', ability: {all: {manage: "1"}})
+        @super_user = build(:user, name: 'User', role_id: super_role.id)
 
-				@ability = Ability.new(@super_user)
-			end
+        @ability = Ability.new(@super_user)
+      end
 
-			it	"user can manage all" do
-				expect(@ability.can?(:manage, :all)).to be_truthy
-				expect(@ability.can?(:manage, Tender)).to be_truthy
-				expect(@ability.can?(:manage, User)).to be_truthy
-			end
-		end
+      it  "user can manage all" do
+        expect(@ability.can?(:manage, :all)).to be_truthy
+        expect(@ability.can?(:manage, Tender)).to be_truthy
+        expect(@ability.can?(:manage, User)).to be_truthy
+      end
+    end
 
-		context	"when set read only ability" do
-			before :all do
-				read_only_role = create(:role, name: 'Read only role', ability: {all: {read: "1"}})
-    		@read_only_user = build(:user, name: 'Read only user', role_id: read_only_role.id)
+    context "when set read only ability" do
+      before :all do
+        read_only_role = create(:role, name: 'Read only role', ability: {all: {read: "1"}})
+        @read_only_user = build(:user, name: 'Read only user', role_id: read_only_role.id)
 
-				@ability = Ability.new(@read_only_user)
-			end
+        @ability = Ability.new(@read_only_user)
+      end
 
-			it "user can't create anything" do
-				expect(@ability.can?(:create, Comment)).to be_falsy
-				expect(@ability.can?(:create, Tender)).to be_falsy
-			end
+      it "user can't create anything" do
+        expect(@ability.can?(:create, Comment)).to be_falsy
+        expect(@ability.can?(:create, Tender)).to be_falsy
+      end
 
-			it "user can't update anything" do
-				expect(@ability.can?(:update, Role)).to be_falsy
-				expect(@ability.can?(:update, Tender)).to be_falsy
-			end
+      it "user can't update anything" do
+        expect(@ability.can?(:update, Role)).to be_falsy
+        expect(@ability.can?(:update, Tender)).to be_falsy
+      end
 
-			it "user can't destroy anything" do
-				expect(@ability.can?(:destroy, User)).to be_falsy
-				expect(@ability.can?(:destroy, Role)).to be_falsy
-			end
+      it "user can't destroy anything" do
+        expect(@ability.can?(:destroy, User)).to be_falsy
+        expect(@ability.can?(:destroy, Role)).to be_falsy
+      end
 
-			it "user can read all" do
-				expect(@ability.can?(:read, :all)).to be_truthy
-			end
-		end
+      it "user can read all" do
+        expect(@ability.can?(:read, :all)).to be_truthy
+      end
+    end
 
-		context	"when set read tenders and create comments" do
-			before :all do
-				tender_member_role = create(:role, name: 'Tender member role', ability: {tender: {read: "1"}, comment: {create: "1"}})
-    		tender_member_user = build(:user, name: 'Tender member user', role_id: tender_member_role.id)
+    context "when set read tenders and create comments" do
+      before :all do
+        tender_member_role = create(:role, name: 'Tender member role', ability: {tender: {read: "1"}, comment: {create: "1"}})
+        tender_member_user = build(:user, name: 'Tender member user', role_id: tender_member_role.id)
 
-				@ability = Ability.new(tender_member_user)
-			end
+        @ability = Ability.new(tender_member_user)
+      end
 
-			it "user can read tenders" do
-				expect(@ability.can?(:read, Tender)).to be_truthy
-			end
+      it "user can read tenders" do
+        expect(@ability.can?(:read, Tender)).to be_truthy
+      end
 
-			it "user can't create tenders" do
-				expect(@ability.can?(:create, Tender)).to be_falsy
-			end
+      it "user can't create tenders" do
+        expect(@ability.can?(:create, Tender)).to be_falsy
+      end
 
-			it "user can't read comments" do
-				expect(@ability.can?(:read, Comment)).to be_falsy
-			end
+      it "user can't read comments" do
+        expect(@ability.can?(:read, Comment)).to be_falsy
+      end
 
-			it "user can create comments" do
-				expect(@ability.can?(:create, Comment)).to be_truthy
-			end
-		end
+      it "user can create comments" do
+        expect(@ability.can?(:create, Comment)).to be_truthy
+      end
+    end
 
-	end
+  end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 describe User do
-	describe "#full_name" do
-	  it 'returns position or name (if position is blank)' do
-	    user_with_position    = build(:user, name: 'Paul George', position: 'Forward')
-	    user_without_position = build(:user, name: 'Steve Nash')
+  describe "#full_name" do
+    it 'returns position or name (if position is blank)' do
+      user_with_position    = build(:user, name: 'Paul George', position: 'Forward')
+      user_without_position = build(:user, name: 'Steve Nash')
 
-	    expect(user_with_position.full_name).to eq user_with_position.position
-	    expect(user_without_position.full_name).to eq user_without_position.name
-	  end
+      expect(user_with_position.full_name).to eq user_with_position.position
+      expect(user_without_position.full_name).to eq user_without_position.name
+    end
   end
 
   describe "#is_admin?" do
-  	it 'check is user has all permissions' do
-  		expect(@user.is_admin?).to be_falsy
-  		expect(@admin.is_admin?).to be_truthy
-  	end
+    it 'check is user has all permissions' do
+      expect(@user.is_admin?).to be_falsy
+      expect(@admin.is_admin?).to be_truthy
+    end
   end
 end
 
