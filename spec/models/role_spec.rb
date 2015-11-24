1e31d1c5acb0e3e6cc2cf2587dcd553f9d7a6f1b
Alexey Krasman <akrasman@gmail.com>
2015-11-24 15:12:59 +0300

    Role specs refactoring.

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -4,7 +4,7 @@ describe Role do
 
 	describe "Ability" do
 
-		context	"set all abilities" do
+		context	"when set all abilities" do
 			before :all do
     		super_role = create(:role, name: 'Super role', ability: {all: {manage: "1"}})
     		@super_user = build(:user, name: 'User', role_id: super_role.id)
@@ -12,14 +12,14 @@ describe Role do
 				@ability = Ability.new(@super_user)
 			end
 
-			it	"cam manage all" do
+			it	"user can manage all" do
 				expect(@ability.can?(:manage, :all)).to be_truthy
 				expect(@ability.can?(:manage, Tender)).to be_truthy
 				expect(@ability.can?(:manage, User)).to be_truthy
 			end
 		end
 
-		context	"set read only ability" do
+		context	"when set read only ability" do
 			before :all do
 				read_only_role = create(:role, name: 'Read only role', ability: {all: {read: "1"}})
     		@read_only_user = build(:user, name: 'Read only user', role_id: read_only_role.id)
@@ -27,27 +27,27 @@ describe Role do
 				@ability = Ability.new(@read_only_user)
 			end
 
-			it "can't create anything" do
+			it "user can't create anything" do
 				expect(@ability.can?(:create, Comment)).to be_falsy
 				expect(@ability.can?(:create, Tender)).to be_falsy
 			end
 
-			it "can't update anything" do
+			it "user can't update anything" do
 				expect(@ability.can?(:update, Role)).to be_falsy
 				expect(@ability.can?(:update, Tender)).to be_falsy
 			end
 
-			it "can't destroy anything" do
+			it "user can't destroy anything" do
 				expect(@ability.can?(:destroy, User)).to be_falsy
 				expect(@ability.can?(:destroy, Role)).to be_falsy
 			end
 
-			it "can read all" do
+			it "user can read all" do
 				expect(@ability.can?(:read, :all)).to be_truthy
 			end
 		end
 
-		context	"set read tenders and create comments" do
+		context	"when set read tenders and create comments" do
 			before :all do
 				tender_member_role = create(:role, name: 'Tender member role', ability: {tender: {read: "1"}, comment: {create: "1"}})
     		tender_member_user = build(:user, name: 'Tender member user', role_id: tender_member_role.id)
@@ -55,19 +55,19 @@ describe Role do
 				@ability = Ability.new(tender_member_user)
 			end
 
-			it "can read tenders" do
+			it "user can read tenders" do
 				expect(@ability.can?(:read, Tender)).to be_truthy
 			end
 
-			it "can't create tenders" do
+			it "user can't create tenders" do
 				expect(@ability.can?(:create, Tender)).to be_falsy
 			end
 
-			it "can't read comments" do
+			it "user can't read comments" do
 				expect(@ability.can?(:read, Comment)).to be_falsy
 			end
 
-			it "can create comments" do
+			it "user can create comments" do
 				expect(@ability.can?(:create, Comment)).to be_truthy
 			end
 		end
